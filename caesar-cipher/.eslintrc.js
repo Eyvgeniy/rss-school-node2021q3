37102df66eda24a88bbf55c62cc188c5aca0309f
Eyvgeniy <antonm33@yandex.ru>
2021-05-04 20:49:37 +0200

    init: added eslint, package.json, gitignore

---
+++ caesar-cipher/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  plugings: ['jest'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
