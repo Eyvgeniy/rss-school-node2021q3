00a070fa5192b10bd3a353f6d03ea6ce8bfc2d86
Eyvgeniy <antonm33@yandex.ru>
2021-05-06 16:30:20 +0200

    feat: added example call to help

--- caesar-cipher/src/bin/caesar-cipher-cli.js
+++ caesar-cipher/src/bin/caesar-cipher-cli.js
@@ -1,15 +1,23 @@
 #!/bin/env node
 
 import { program } from 'commander';
-import caesarCipher from '../streams.js';
+import caesarCipher from '../pipeStreams.js';
 
 program.version('0.0.1');
 program
   .description('Encode/Decode text using Caesar cipher')
-  .option('-s, --shift <shift>', 'a shift')
+  .option('-s, --shift <shift>', 'a shift, value must be Integer, mandatory option')
   .option('-i, --input <input>', 'a input file')
   .option('-o, --output <output>', 'a output file')
-  .option('-a, --action <action>', 'an action encode/decode')
+  .option('-a, --action <action>', 'an action, value: encode/decode, mandatory option')
   .action(() => caesarCipher(program.opts()));
+
+program.addHelpText(
+  'after',
+  `
+
+  Example call:
+    $ node caesar-cipher-cli -s 7 -a encode -i input.txt -o output.txt`,
+);
 
 program.parse(process.argv);
